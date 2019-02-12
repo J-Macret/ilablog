e5da482279cc13f49a04f5d1106e1193b3c5cf01
Unknown <macret.jonathan@gmail.com>
2019-02-12 21:07:17 +0100

    lolé

--- index.php
+++ index.php
@@ -11,22 +11,27 @@
                     <div class="hr"></div>
                     <p>Bienvenue sur mon blog dédié au Roleplay !<br> Les personnages auxquels sont liés les textes viennent de l'univers de Final Fantasy XIV, serveur Ragnarok !<br>Bonne lecture ! :D</p><br>
                     
-                    <h1>Les trois derniers articles :</h1>
-                    <div class="last-article"><?php
-                        $req = $db->query('SELECT * FROM article ORDER BY id DESC LIMIT 3 ');
-                        $articles = $req->fetchAll();
-                        
-                        foreach ($articles as $article): ?>
+                    <h1>Mes deux derniers articles :</h1><div class="hr hr100"></div><br>
+                    <div class="last-article">
+                       
+                       
+                       <div class="card-deck"><?php
+                            $req = $db->query('SELECT * FROM article ORDER BY id DESC LIMIT 2 ');
+                            $articles = $req->fetchAll();
                             
-                            <div class="card">
-                                <h1><?= $article['name']; ?></h1>
-                                <h4><?= $article['nom_personnage']; ?></h4>
-                                <div class="hr"></div>
-                                <img src="http://placehold.it/500x300">
-                                <p><?php echo mb_strimwidth($article['content'], 0, 250, "..."); ?><br>Par : <?= $article['author']; ?></p>
-                                <a href="single_article.php?id=<?= $article['id'];?>">Voir l'article en entier</a>
-                            </div>                            
-                        <?php endforeach; ?>
+                            
+                            foreach ($articles as $article): ?>
+                                
+                                <div class="card">
+                                    <h1><?= $article['name']; ?></h1>
+                                    <h4><?= $article['nom_personnage']; ?></h4>
+                                    <div class="hr"></div>
+                                    <img src="http://placehold.it/500x300"><div class="hr"></div>
+                                    <p><?php echo mb_strimwidth($article['content'], 0, 200, "..."); ?></p><br>
+                                    <p>Par : <?= $article['author']; ?></p><br>
+                                    <a href="single_article.php?id=<?= $article['id'];?>">Voir l'article en entier</a>
+                                </div>                            
+                            <?php endforeach; ?></div>
                     </div>
                 </div>
             </div>
--- single_article.php
+++ single_article.php
@@ -7,24 +7,25 @@ require_once('php/function.php');
 
 $article = getArticle($db,1, $_GET['id']); ?>
 
+
 <div class="container">
         <div class="table">
             <div class="cell">
                 <div class="content">
                 
                 <article>
-                    <h1><?= $article->name ?></h1>
-                    <h2><?= $article->nom_personnage ?></h2>
-                    <p><?= $article->content ?></p>
-                    <h3>Par <?= $article->author ?></h3>
+                    <h1><?= $article->name ?></h1><div class="hr"></div>
+                    <h2 class="personnage"><?= $article->nom_personnage ?></h2><div class="hr"></div>
+                    <span class="content"><p class="content-article"><?= $article->content ?></p></span><br>
+                    <h3 class="auteur">Par <?= $article->author ?></h3>
                 </article>
                 </div>
                 
                 
                 <?php if(isset($_SESSION['admin']) AND !empty($_SESSION['admin'])): ?>
                 <div class="gestion-article">
-                    <a href="admin/delete_article.php?id=<?= $article->id ?>"><h2>Supprimer cet article</h2></a>
-                    <a href="admin/edit_article.php?id=<?= $article->id ?>"><h2>Modifier cet article</h2></a>
+                    <a onclick='return confirm("Êtes vous sûr ?");' href="admin/delete_article.php?id=<?= $article->id ?>"><h2>Supprimer cet article</h2></a>
+                    <a href="admin/edit_article.php?id=<?= $article->id ?>" ><h2>Modifier cet article</h2></a>
                     <a href="admin/admin1107_1402.php"><h2>Espace Admin</h2></a>
                 </div>
                 <?php endif; ?>
@@ -37,5 +38,6 @@ $article = getArticle($db,1, $_GET['id']); ?>
 <script src="js/jQuery.js"></script>
 <script src="js/jquery.min.js"></script>
 <script src="js/menu.js"></script>
+
 
 </html>
